f92b1b6b5ab74ffc30a2e3ae36f3b7c136392ec3
cameron-green-1 <cameron.green@voxelstudio.co.uk>
2024-04-10 20:13:13 +0100

    Basic styles

--- renderer/pages/index.js
+++ renderer/pages/index.js
@@ -1,13 +1,28 @@
-import { useEffect, useState } from 'react';
-import styles from '../styles/Home.module.css';
+import { useEffect, useState } from 'react'
+import { convertFileToBase64 } from '../utils/helpers'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
-    useEffect(() => {
-        // on mount
-    }, []);
-    return (
-        <div className={styles.wrapper}>
-            <h1>Nextron Boilerplate</h1>
-        </div>
-    );
+  const [image, setImage] = useState(null)
+  useEffect(() => {
+    // on mount
+  }, [])
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const data = new FormData(e.target)
+    const { image, prompt } = Object.fromEntries(data)
+    const base64Image = await convertFileToBase64(image)
+    setImage(base64Image)
+  }
+  return (
+    <div className={styles.wrapper}>
+      <h1>AI Photo Booth</h1>
+      <form onSubmit={handleSubmit}>
+        <input type='file' accept='image/*' id='inputImage' name='image' />
+        <textarea name='prompt' id='inputPrompt' cols='30' rows='10'></textarea>
+        <button type='submit'>Upload</button>
+      </form>
+      {image === null ? null : <img src={image} alt='uploaded' />}
+    </div>
+  )
 }
